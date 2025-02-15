c613e5bb27d0d7916a39e249485852f100ff77fe
Nutakki Krishna vamsi <128046865+krishnavamsi-nk@users.noreply.github.com>
2025-02-15 11:25:02 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -77,5 +77,5 @@ def index():
 
 if __name__ == '__main__':
     print("🚀 Flask server is starting...")  # Debugging
-    port = int(os.environ.get("PORT", 10000))  # Use Render's dynamic PORT
+    port = int(os.getenv("PORT", 5000))  # Use Render's PORT
     app.run(host="0.0.0.0", port=port, debug=True)
